6bb5045f70d05fd00ff6f8f877dcb5ce4d96c150
Zawadi Sanga <zawadisanga397@gmail.com>
2026-05-23 19:38:12 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'zass-v1';
+const urls = ['/', '/dashboard.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urls)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
